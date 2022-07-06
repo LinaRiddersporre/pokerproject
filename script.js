907b7e9577796c3146c014c26ba24ff85217131d
Lina <linariddersporre@gmail.com>
2022-07-06 14:14:45 +0200

    create button

--- script.js
+++ script.js
@@ -22,3 +22,15 @@ for(let i=0; i<5; i++){
 
 
 console.log(cards);
+
+let randomNumberButton = document.createElement('button');
+let wrapper = document.querySelector('.wrapper');
+wrapper.appendChild(randomNumberButton);
+
+randomNumberButton.addEventListener('click', () => {
+    number()
+    let num = document.createElement('p');
+    wrapper.appendChild(num);
+    num.innerHTML = cards;
+});
+
